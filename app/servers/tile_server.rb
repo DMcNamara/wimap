4322cdd8e94e15f5f4573d2b50e812ff7062d792
surette <scott@scottsurette.com>
2014-02-08 14:38:59 -0500

    get_tile function

---
+++ app/servers/tile_server.rb
@@ -0,0 +1,8 @@
+def get_tile(lat_deg, lng_deg, zoom)
+  lat_rad = lat_deg/180 * Math::PI
+  n = 2.0 ** zoom
+  x = ((lng_deg + 180.0) / 360.0 * n).to_i
+  y = ((1.0 - Math::log(Math::tan(lat_rad) + (1 / Math::cos(lat_rad))) / Math::PI) / 2.0 * n).to_i
+ 
+  {:x => x, :y =>y}
+end
