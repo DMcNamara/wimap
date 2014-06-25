f4dcaea59bf052b54f9bb71bc04a0ae3ddeb71cb
Plasmarobo <plasmarobo@gmail.com>
2014-06-25 19:43:33 -0400

    API expansion/sync

--- app/views/sites/show.json.jbuilder
+++ app/views/sites/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @site, :id, :name, :created_at, :updated_at
+json.extract! @site, :id, :name, :latitude, :longitude :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624005321) do
+ActiveRecord::Schema.define(version: 20140625234113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20140624005321) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "tracks", force: true do |t|
